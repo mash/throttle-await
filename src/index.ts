d47aaa6900b6d85bf77c1e391f71332ecec8c4b6
Masakazu Ohtsuka <o.masakazu@gmail.com>
2020-05-13 00:18:11 +0300

    throttle starts to do something interesting

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,48 @@
-// class Throttle {
-//   constructor (fn, timer, max) {
-//   }
-//   interval() {
-//   }
-// }
+let sleep = (ms) => {
+  return new Promise((resolve, _) => setTimeout(resolve, ms));
+}
+
+class Throttled {
+  bucket :number;
+  interval :number;
+  max :number;
+  fn;
+  timer;
+  constructor(fn, interval :number, max :number, initial :number = max) {
+    this.bucket = initial;
+    this.fn = fn;
+    this.interval = interval;
+    this.max = max;
+    this.timer = setTimeout(this.periodically.bind(this), interval);
+  }
+  periodically() :void {
+    this.bucket += this.max;
+  }
+  async take() {
+    if (this.bucket > 0) {
+      this.bucket --;
+      return;
+    }
+    while (this.bucket == 0) {
+      await sleep(this.interval / 10);
+    }
+    this.bucket --;
+    return;
+  }
+  async call() {
+    await this.take();
+    return this.fn.call(null);
+  }
+  cancel() :void {
+    clearTimeout(this.timer);
+    this.timer = null;
+  }
+}
 
 // Parameter fn should not use "this", for simplicity.
-function throttle() {
-  return true;
+function throttle(fn, interval :number, max :number, initial :number = max) {
+  let throttled = new Throttled(fn, interval, max, initial);
+  return throttled;
 }
 
 export default throttle;
--- test/test.ts
+++ test/test.ts
@@ -2,7 +2,23 @@ import throttle from "../src/index";
 import * as tape from 'tape';
 
 tape('throttle', (t) => {
-  t.ok(throttle());
-  t.end();
-});
+  (async () => {
+    let fn = () => new Date();
+    let throttled = throttle(fn, 1000, 2); // 2 times per second
 
+    let first = await throttled.call();
+    console.log(`first = ${first.getTime()}`);
+
+    let second = await throttled.call();
+    console.log(`second = ${second.getTime()}`);
+    t.ok(second - first < 500, '2nd call happens immediately');
+    
+    let third = await throttled.call();
+    console.log(`third = ${third.getTime()}`);
+    t.ok(third - second > 500, '3rd call is throttled');
+
+    throttled.cancel();
+    
+    t.end();
+  })();
+});
